807bfe099e8b32999026ba46fda6c16401782faf
Edson Valenzuela <edsonvalher@gmail.com>
2021-04-12 16:18:09 -0600

    mi primer commit

---
+++ app.js
@@ -0,0 +1,14 @@
+require('colors')
+const { mostrarMenu, pausa } = require('./helpers/mensaje')
+console.clear()
+
+const main = async () => {
+    let opt = ''
+    do {
+        opt = await mostrarMenu()
+        console.log({ opt })
+        if (opt !== '0') await pausa()
+    } while (opt !== '0');
+}
+
+main()
