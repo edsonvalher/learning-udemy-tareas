7efd20cf95c163b1f428fc1ef4ec79ba67e55173
Edson Valenzuela <edsonvalher@gmail.com>
2021-04-13 08:27:14 -0600

    carga las opciones interactivamente

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 require('colors')
+const { inquirerMenu } = require('./helpers/inquirer')
 const { mostrarMenu, pausa } = require('./helpers/mensaje')
 console.clear()
 
 const main = async () => {
     let opt = ''
     do {
-        opt = await mostrarMenu()
+        opt = await inquirerMenu()
         console.log({ opt })
         if (opt !== '0') await pausa()
     } while (opt !== '0');
